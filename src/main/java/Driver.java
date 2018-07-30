ffa8d148441555a0e04e1298d7c81dbb95f6178c
Jianyi_1001 <jianyishi1995@outlook.com>
2018-07-29 22:01:42 -0700

    add more comment

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,12 +7,12 @@ public class Driver {
         UnitSum sum = new UnitSum();
 
         //args0: dir of transition.txt
-        //args1: dir of PageRank.txt
-        //args2: dir of unitMultiplication result
+        //args1: dir of PageRank.txt, prefix PrX
+        //args2: dir of unitMultiplication result, subPr
         //args3: times of convergence
         String transitionMatrix = args[0];
         String prMatrix = args[1];
-        String unitState = args[2];
+        String unitState = args[2]; 
         int count = Integer.parseInt(args[3]);
         for(int i=0;  i<count;  i++) {
             String[] args1 = {transitionMatrix, prMatrix+i, unitState+i};
